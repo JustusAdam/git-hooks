fe1bf925326ccc2b865d06829e0c87012f95240d
Justus Adam <dev@justus.science>
2016-06-29 18:24:07 +0200

    extended stylish-haskell description

--- pre-commit/stylish-haskell.sh
+++ pre-commit/stylish-haskell.sh
@@ -1,4 +1,8 @@
 # Uses the stylish-haskell executable to format your code when you commit new changes
 
+# Recursively searches for files ending in .hs in the directories
+# "app" and "src" (standard source directories for stack)
+# If your directory structure is different you can substitute "app" and "src"
+# for arbitrarily many other directories.
 stylish-haskell -i `find app src -name "*.hs"`
 git add -u
